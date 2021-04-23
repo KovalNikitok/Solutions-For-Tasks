c456002b54e18abe42479f6fabf06a7e9ac8d5c0
KovalNikitok <52690668+KovalNikitok@users.noreply.github.com>
2021-04-23 16:29:19 +0600

    Update acync function

--- JSTask.js
+++ JSTask.js
@@ -5,43 +5,12 @@
 		}
 		return formatingTime;
 	}
-	function scrollWindow(trackLengthLast){//функция скроллинга с возвращаемым массивом о треках
-		setTimeout(() => { 
-			window.scrollBy(0,9000);
-			var audioCatalog = document.getElementsByClassName('audio_row audio_row_with_cover _audio_row');
-			if(trackLengthLast!=audioCatalog.length&&isFinal!=true){
-				scrollWindow(audioCatalog.length);
-			}
-			clearTimeout();
-			return audioCatalog; 
-		}, 400);//задержка, необходимая при моей конфигурации для подгрузки новых записей
-	}
 	function timeCorrection(time, format){//функция коррекции времени
 		return time%format;
 	}
-
-	function finalResponse(quantity, time){//функция-конструктор для console.table
-		this.quantity=quantity;
-		this.time=time;
-	}
-
-	function getTime(catalog){//функция получения времени
-		var audioRow=[],  tracksLength=[]; //Создаём массивы для хранения данных из audioCatalog
-		var	seconds=0,	minutes=0,	hours=0,  days=0,  months=0; //Переменные для хранения времени продолжительности треков
-		
-		for(var i=0; i<catalog.length;i++){
-		//Получаем массив с длительностью треков типа [часы, минуты, секунды] (часы появляются только в случае, если продолжительность трека больше 60 минут)
-			audioRow[i]=catalog[i].innerText.split('\n');
-			tracksLength[i]=audioRow[i][audioRow[i].length-1].split(':');
-		} 
-		for(var i=0;i<tracksLength.length;i++){
-			seconds+=+tracksLength[i][tracksLength[i].length-1];
-			minutes+=+tracksLength[i][tracksLength[i].length-2];
-			if(tracksLength[i].length===3){
-				hours+=+tracksLength[i][tracksLength[i].length-3];
-			}
-		}//Получаем сумму продолжительности по времени всех треков в секундах, минутах и часах(если есть)
-		
+	function getTimesString(hours,minutes,seconds){
+		var days=0,months=0;
+		//функция для перевода времени к формату; месяцы, дни, часы, минуты, секунды
 		minutes=timesFormated(seconds,minutes,60);
 		seconds=timeCorrection(seconds,60);
 		hours=timesFormated(minutes,hours,60);
@@ -50,18 +19,53 @@
 		hours=timeCorrection(hours,24);
 		months=timesFormated(days,months,30);
 		days=timeCorrection(days,30);
-		//форматирование и корректировка времени функциями
-		isFinal=true;
-		return console.table([new finalResponse(tracksLength.length,((months>0)?(months+':'+days+':'+hours+':'+minutes+':'+seconds):
+		
+		return ((months>0)?(months+':'+days+':'+hours+':'+minutes+':'+seconds)://формируем строку(в зависимотси от имеющихся данных) вида: месяцы:дни:часы:минуты:секунды
 		(days>0)?(days+':'+hours+':'+minutes+':'+seconds):(hours>0)?(hours+':'+minutes+':'+seconds):
-		(minutes+':'+seconds)))]); //возвращаем console.table с количеством треков и суммарной их продолжительностью
+		(minutes+':'+seconds));
+	}
+	async function scrollWindow(trackLengthLast){
+		//функция скроллинга с получением конечного результата
+		var scrolling=new Promise((resolve, reject) => {
+			setTimeout(() => { 
+				window.scrollBy(0,9000);//прокручиваем экран
+				var audioCatalog = document.getElementsByClassName('audio_row audio_row_with_cover _audio_row');//получаем текущие треки
+				if(trackLengthLast!=audioCatalog.length){
+					//автоматически прокручиваем экран, вызывая рекурсию, пока не прогрузятся все треки
+					scrollWindow(audioCatalog.length);
+				}
+				resolve(audioCatalog);
+			}, 400)//задержка, необходимая при моей конфигурации для подгрузки новых запис
+		});
+		getTime(await scrolling);//вызов функции получения времени
+	}
+
+	function finalResponse(quantity, time){//функция-конструктор для console.table
+		this.quantity=quantity;
+		this.time=time;
+	}
+
+	function getTime(catalog){//функция получения времени
+		var audioRow=[],  tracksLength=[]; //Создаём массивы для хранения данных из catalog
+		var	seconds=0,	minutes=0,	hours=0; //Переменные для хранения времени продолжительности треков
+		for(var i=0; i<catalog.length;i++){
+		//Получаем массив с длительностью треков типа [часы, минуты, секунды] (часы появляются только в случае, если продолжительность трека больше 60 минут)
+			audioRow[i]=catalog[i].innerText.split('\n');
+			tracksLength[i]=audioRow[i][audioRow[i].length-1].split(':');
+		} 
+		for(var i=0;i<tracksLength.length;i++){
+			//Получаем сумму продолжительности по времени всех треков в секундах, минутах и часах(если есть)
+			seconds+=+tracksLength[i][tracksLength[i].length-1];
+			minutes+=+tracksLength[i][tracksLength[i].length-2];
+			if(tracksLength[i].length===3){
+				hours+=+tracksLength[i][tracksLength[i].length-3];
+			}
+		}
+		var timesString=getTimesString(hours,minutes,seconds);//вызываем функцию преобразования часов, минут и секунд в строку формата (месяцы:дни:часы:минуты:секунды)
+		return console.table([new finalResponse(tracksLength.length,timesString)]);
+		//возвращаем console.table с количеством треков и суммарной их продолжительностью
 	}	
-		var audioCatalog = document.getElementsByClassName('audio_row audio_row_with_cover _audio_row');//Принимаем все элементы по классу
-		var timeDelay=15000; 
-		//15 секунд для задержки, необходимые для корректного скролла страницы и подгрузки записей (успевает получить 3500 треков, при моей конфигурации пк/сети)
-		var isFinal=false;
-		window.scrollBy(0,9000); 
-		scrollWindow(audioCatalog.length);//вызов функции скроллинга audioCatalog=
-		setTimeout(() => { 			
-			getTime(audioCatalog);
-		}, timeDelay);//вызов функции получения количества треков и их суммарной продолжительности
+		//Принимаем все элементы по классу
+		var audioCatalog = document.getElementsByClassName('audio_row audio_row_with_cover _audio_row');
+		window.scrollBy(0,9000); //первая прокрутка экрана для попытки загрузить новые треки, если таковые имеются
+		scrollWindow(audioCatalog.length);//вызов функции скроллинга и получения результата
