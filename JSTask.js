062d1a0730e4ba55cb1a4140978ee5a7801ca56d
KovalNikitok <52690668+KovalNikitok@users.noreply.github.com>
2021-04-23 10:43:55 +0600

    Added async window scrolling func
    Update function scrollWindow(...) to asynchronous  setTimeout method with time delay for loading new tracks

--- JSTask.js
+++ JSTask.js
@@ -5,16 +5,22 @@
 		}
 		return formatingTime;
 	}
-	function scrollWindow(){//функция скроллинга
-		for(var i=0;i<10;i++){
-			window.scrollBy(0,1920);
-		}
+	function scrollWindow(trackLengthLast){//функция скроллинга с возвращаемым массивом о треках
+		setTimeout(() => { 
+			window.scrollBy(0,9000);
+			var audioCatalog = document.getElementsByClassName('audio_row audio_row_with_cover _audio_row');
+			if(trackLengthLast!=audioCatalog.length&&isFinal!=true){
+				scrollWindow(audioCatalog.length);
+			}
+			clearTimeout();
+			return audioCatalog; 
+		}, 400);//задержка, необходимая при моей конфигурации для подгрузки новых записей
 	}
 	function timeCorrection(time, format){//функция коррекции времени
 		return time%format;
 	}
 
-	function finalResponse(quantity, time){//объект для console.table
+	function finalResponse(quantity, time){//функция-конструктор для console.table
 		this.quantity=quantity;
 		this.time=time;
 	}
@@ -22,6 +28,7 @@
 	function getTime(catalog){//функция получения времени
 		var audioRow=[],  tracksLength=[]; //Создаём массивы для хранения данных из audioCatalog
 		var	seconds=0,	minutes=0,	hours=0,  days=0,  months=0; //Переменные для хранения времени продолжительности треков
+		
 		for(var i=0; i<catalog.length;i++){
 		//Получаем массив с длительностью треков типа [часы, минуты, секунды] (часы появляются только в случае, если продолжительность трека больше 60 минут)
 			audioRow[i]=catalog[i].innerText.split('\n');
@@ -44,11 +51,17 @@
 		months=timesFormated(days,months,30);
 		days=timeCorrection(days,30);
 		//форматирование и корректировка времени функциями
-		
+		isFinal=true;
 		return console.table([new finalResponse(tracksLength.length,((months>0)?(months+':'+days+':'+hours+':'+minutes+':'+seconds):
 		(days>0)?(days+':'+hours+':'+minutes+':'+seconds):(hours>0)?(hours+':'+minutes+':'+seconds):
 		(minutes+':'+seconds)))]); //возвращаем console.table с количеством треков и суммарной их продолжительностью
-	}
-		scrollWindow();//вызов функции скроллинга
+	}	
 		var audioCatalog = document.getElementsByClassName('audio_row audio_row_with_cover _audio_row');//Принимаем все элементы по классу
-		getTime(audioCatalog);//вызов функции получения количества треков и их суммарной продолжительности
+		var timeDelay=15000; 
+		//15 секунд для задержки, необходимые для корректного скролла страницы и подгрузки записей (успевает получить 3500 треков, при моей конфигурации пк/сети)
+		var isFinal=false;
+		window.scrollBy(0,9000); 
+		scrollWindow(audioCatalog.length);//вызов функции скроллинга audioCatalog=
+		setTimeout(() => { 			
+			getTime(audioCatalog);
+		}, timeDelay);//вызов функции получения количества треков и их суммарной продолжительности
